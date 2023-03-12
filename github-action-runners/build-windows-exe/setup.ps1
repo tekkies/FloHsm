f85b136017b442d5119417e63e796e2ad95219f1
Andy Joiner <andy.joiner.0101@gmail.com>
2023-03-12 08:27:32 +0000

    Built out vagrant box

--- github-action-runners/build-windows-exe/setup.ps1
+++ github-action-runners/build-windows-exe/setup.ps1
@@ -16,8 +16,16 @@ choco install -y googlechrome
 choco install -y firefox
 choco install -y 7zip
 choco install -y notepadplusplus
-#choco install git -y -params '"/GitAndUnixToolsOnPath /NoAutoCrlf"'
- 
+choco install git -y -params '"/GitAndUnixToolsOnPath /NoAutoCrlf"'
+
+choco install -y --version 3.11.0 python
+refreshenv
+python.exe -m pip install --upgrade pip
+pip install ply==3.11
+pip install Mako==1.2.4
+pip install pyinstaller==5.8.0
+refreshenv
+
+
 Install-ChocolateyPinnedTaskBarItem "${env:ProgramFiles(x86)}\Google\Chrome\Application\chrome.exe"
 Install-ChocolateyPinnedTaskBarItem "${env:ProgramFiles(x86)}\Mozilla Firefox\firefox.exe"
- 
